9bd2b6acf32df41a23428ed27609d52d0df5347c
Kushagra976 <kushagragaur97@gmail.com>
2026-06-03 01:18:36 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import dill 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -24,7 +25,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         
 
@@ -32,8 +33,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train) # Train model
+            
+            para=param[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             # Make predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
